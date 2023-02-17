55be6358ea5f1ddf41481bc0434eff2dc78901a2
Abdallah Hussein <abiza2013@gmail.com>
2023-02-17 09:42:22 -0800

    2222

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-/* more headers goes there
- * hea */
-
-/* betty style doc for function main goes there */
+/**
+ * more headers goes there
+ *
+ *  betty style doc for function main goes there 
+ */
 int main(void)
 {
 	int i;
