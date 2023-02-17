aa228b5f3e99545b05573ce19fccacd68073e964
Abdallah Hussein <abiza2013@gmail.com>
2023-02-17 10:20:58 -0800

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar (i + '0');
+		if (n < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
