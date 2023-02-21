ebd63552e3efd73d968e4bdacd4eec777619d1b7
Abdallah Hussein <abiza2013@gmail.com>
2023-02-21 15:57:01 -0800

    vv

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - hehe
+ * Description: main fun
+ * @a: int a
+ * @b: int b
+ * Return: bye bye
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
