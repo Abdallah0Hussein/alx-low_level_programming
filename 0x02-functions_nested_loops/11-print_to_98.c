3aa8798c884101bc0fd8d275332809f6908f0242
Abdallah Hussein <abiza2013@gmail.com>
2023-02-21 17:37:42 -0800

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,6 +14,10 @@ void print_to_98(int n)
 		for (; n >= 98; n--)
 		{
 			printf("%d, ", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
 		}
 
 	}
@@ -22,6 +26,10 @@ void print_to_98(int n)
 		for (; n <= 98; ++n)
 		{
 			printf("%d, ", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
 		}
 	}
 	printf("\n");
